eec699f9375602b12b680d9a3fcdd6691bd696e8
William Rogge <williamroggeiv@gmail.com>
2020-05-13 22:04:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(num)
   return false if !num.integer?
   return false if num < 2  
-  return true is num == 2  
+  return true if num == 2  
   (2..num-1).each {|x| return false if num % x==0}
   true
 end
