9fa9eb2fea49dbb65d400a4f496d4fe1d1be6e14
William Rogge <williamroggeiv@gmail.com>
2020-05-13 22:08:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(num)
+  return false if !num.integer?
+  return false if num < 2  
+  return true if num == 2  
+  (2..num-1).each {|x| return false if num % x==0}
+  true
+end
+
